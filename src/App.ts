38c2465ebf1046792e2ea68414c610fc71403d54
Rafael Ribeiro <Rafaelribeiro12@live.com>
2023-12-21 00:27:58 -0300

    Finding how to instantiate one slot

--- src/App.ts
+++ src/App.ts
@@ -2,15 +2,37 @@ import Phaser from "phaser";
 
 export default class AncientDices extends Phaser.Scene {
   preload() {
-    this.load.image("melee", "src/assets/Melee.png");
-    this.load.image("Defmelee", "src/assets/DefMelee.png");
-    this.load.image("ranged", "src/assets/Ranged.png");
-    this.load.image("Defranged", "src/assets/DefMelee.png");
-    this.load.image("thief", "src/assets/Thief.png");
+    this.load.image("melee", "src/assets/SideDices/Melee.png");
+    this.load.image("Defmelee", "src/assets/SideDices/DefMelee.png");
+    this.load.image("ranged", "src/assets/SideDices/ranged.png");
+    this.load.image("Defranged", "src/assets/SideDices/DefMelee.png");
+    this.load.image("thief", "src/assets/SideDices/Thief.png");
+    this.load.image("roll", "src/assets/RoboticArm/Roll.png");
+    this.load.image("roboticArm", "src/assets/RoboticArm/RoboticArm.png");
+    this.load.image("slot", "src/assets/RoboticArm/Slot.png");
   }
 
   create() {
-    this.add.image(400, 500, "melee");
+    this.add.image(230, 500, "roboticArm");
+    this.add.image(100, 500, "slot");
+    // this.add.image(100, 500, "melee");
+
+    class FirstSlot extends Phaser.GameObjects.Image {
+      constructor(scene) {
+        super(scene, 0, 0, "melee");
+      }
+
+      Place() {
+        this.setActive(true);
+        this.setVisible(true);
+      }
+    }
+
+    const rollButton = this.add.image(500, 500, "roll").setInteractive();
+    rollButton.on("pointerdown", () => {
+      console.log("Sim");
+      FirstSlot;
+    });
   }
 
   update() {}
